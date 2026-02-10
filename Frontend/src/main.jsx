834096fdc691aef5f2a2e248250b1d11704c1ce7
Anuja Dauskar <anujadauskar@gmail.com>
2026-02-10 17:17:16 +0530

    solve description issue and update video issue

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { HelmetProvider } from 'react-helmet-async'
+import { Helmet } from "react-helmet";
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
